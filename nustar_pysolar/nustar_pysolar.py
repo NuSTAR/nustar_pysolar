9de58eb0c34c57f995ae38ddac13f4e77c048f88
Brian Grefenstette <bwgref@jelly.srl.caltech.edu>
2016-08-15 17:21:12 -0700

    Cleaning up some indents/spaces

--- nustar_pysolar/nustar_pysolar.py
+++ nustar_pysolar/nustar_pysolar.py
@@ -4,46 +4,43 @@ import logging
 
 
 def filter_events(evtdata, fpm='A',energy_low=2.5, energy_high=10.):
-    """Do some basic filtering on known bad pixels.
-    
+	"""Do some basic filtering on known bad pixels.
+	
 	Parameters
-    ----------
+	----------
     evtdata: FITS data structure
-        This should be an hdu.data structure from a NuSTAR FITS file.
+		This should be an hdu.data structure from a NuSTAR FITS file.
 
     fpm: {"FPMA" | "FPMB"}
-        Which FPM you're filtering on. Assumes A if not set.
+		Which FPM you're filtering on. Assumes A if not set.
 
     energy_low: float
-        Low-side energy bound for the map you want to produce (in keV).
+		Low-side energy bound for the map you want to produce (in keV).
 
     energy_high: float
-        High-side energy bound for the map you want to produce (in keV).
+		High-side energy bound for the map you want to produce (in keV).
 
-    Returns
+	Returns
     -------
 
 	goodinds: iterable
 		Index of evtdata that passes the filtering.
+	"""
+    
+	logging.basicConfig(filename='nustar_pysolar.log', level=logging.INFO)
+	logging.info('Started')
 
-    """
-
-    logging.basicConfig(filename='nustar_pysolar.log', level=logging.INFO)
-    logging.info('Started')
-
-	# Do some basic filtering
-
-	#	energy_low = 2.5
-	#	energy_high = 10.
 	pilow = (energy_low - 1.6) / 0.04
-	pihigh = (energy_high - 1.60) /  0.04
+	pihigh = (energy_high - 1.6) / 0.04
 
 	# Grade filter
 	grade_filter = ( evtdata['GRADE'] == 0)
 	pi_filter = ( ( evtdata['PI']>pilow ) &  ( evtdata['PI']<pihigh))
-
-	# Hot pixel filters:
+	
+	# Hot pixel filters
+	
 	# FPMA or FPMB
+	
 	if fpm.find('B') == -1 :
 		logging.info("Filtering bad pixels for FPMA.")
 		pix_filter = np.invert( ( (evtdata['DET_ID'] == 2) & (evtdata['RAWX'] == 16) & (evtdata['RAWY'] == 5) |
@@ -68,7 +65,7 @@ def filter_events(evtdata, fpm='A',energy_low=2.5, energy_high=10.):
 
 	logging.info("Found: ", len(goodinds), " good counts.")
 
-    logging.info('Finished')
+	logging.info('Finished')
 
 	return goodinds
 
