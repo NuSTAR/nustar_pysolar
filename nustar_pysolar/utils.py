55ad5d639c51bdc6a9575f2bd854d594313e2288
Brian Grefenstette <bwgref@srl.caltech.edu>
2017-07-12 12:01:02 -0700

    Skyfield cleanup
    Moved a wrapper for the skyfield ephem into utils.py 
    
    Rebased "corection" keywords to "correction", because English.
    
    Updated planning.py to use the utils.py version of the skyfield ephemeris tool.
    Added Astropy time to convert_nustar_time
    
    For convenience to remove the pain of parsing the MET seconds into Astropy time.
    
    Note that I should probably use Skyfield since that'll properly parse the number of leap seconds...the base NuSTAR times don't actually do this.
    Working updates
    
    
    Finished updates to Convert Notebook example

--- nustar_pysolar/utils.py
+++ nustar_pysolar/utils.py
@@ -1,5 +1,5 @@
 
-def convert_nustar_time(t, leap=5):
+def convert_nustar_time(t, leap=5, astropy_time=False):
     '''Converts MET seconds to a datetime object.
     
     Uses the astropy.time.Time method to do the conversion since you have
@@ -14,7 +14,13 @@ def convert_nustar_time(t, leap=5):
     mjdref = 55197*u.d
     met = (t - leap)* u.s + mjdref
     met_datetime = Time(met.to(u.d), format = 'mjd').datetime
-    return met_datetime
+    
+    if astropy_time is True:
+        from astropy.time import Time
+        met_astro = Time(met_datetime)
+        return met_astro
+    else:
+        return met_datetime
 
 def channel_to_keV(pi):
     '''Converts MET seconds to a datetime object.
@@ -42,3 +48,65 @@ def keV_to_channel(keV):
     pi = np.floor((keV - 1.6) / 0.04)
     
     return pi
+
+
+def skyfield_ephem(load_path=None, parallax_correction=False, utc=None):
+    '''Returns skyfield objects used for doing solar planning and conversion.
+    
+    Optional keywords:
+    
+    load_path: Directory location where the bsp files are stored. Defaults to current
+    working directory if None.
+    
+    parallax_corection: Download the latest NuSTAR TLE file and apply the parallax
+    correction based on NuSTAR's position in its orbit. Defaults to "False". If "True"
+    then uses the nustar_pysolar.io TLE methods to parse the closest TLE entry in the
+    NuSTAR database.
+    
+    If you set parallax_correction=True then 
+    
+    Returns:
+    
+    observer, sun, ts
+    
+    The first two are Skyfield objects. "observer" is either geocentric or the NuSTAR
+    location based on the TLE.
+    
+    "ts" is the Skyfield time object.
+    
+    '''
+
+
+
+
+    # Initialize Skyfield ephemeris tools.
+    from skyfield.api import EarthSatellite, Loader
+    from astropy.time import Time
+    import sunpy.sun
+
+    if load_path is None:
+        load_path = './'
+        load=Loader(load_path)
+    else:
+        load=Loader(load_path)
+            
+    ts = load.timescale()
+    planets = load('de436.bsp')
+    earth = planets['Earth']
+    sun = planets['Sun']
+
+
+    if parallax_correction is False:
+        observer = earth
+    else:
+        assert (not utc is None),"Must set UTC when using parallax correction!"            
+        import nustar_pysolar.io as io
+        tlefile = io.download_tle(outdir=load_path)
+        mindt, line1, line2 = io.get_epoch_tle(utc, tlefile)
+        nustar = EarthSatellite(line1, line2)
+        observer = earth + nustar
+
+    ts = load.timescale()
+
+
+    return observer, sun, ts
