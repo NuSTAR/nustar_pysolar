57597255cd65e4a264f2a7e180a80bbb825fec6f
Brian Grefenstette <bwgref@srl.caltech.edu>
2023-07-12 07:33:43 -0700

    Hot fix since I borked __init__.py
    Modules should now be referenceable again

--- nustar_pysolar/__init__.py
+++ nustar_pysolar/__init__.py
@@ -5,3 +5,13 @@
 #     pass  # package is not installed
     
 __version__='0.5'
+
+
+from . import convert
+from . import filter
+from . import io
+from . import map
+from . import planning
+from . import tracking
+from . import utils
+__all__ = ['convert', 'filter','io', 'map', 'planning', 'tracking', 'utils']
